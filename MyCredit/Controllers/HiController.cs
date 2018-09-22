d525b99c3cd6e489c1d478fa928d1aa9f63e6c7c
mighty1234 <Vladlegenda97@gmail.com>
2018-09-22 15:56:32 +0300

    Creating class library for DI and  controller HI

---
+++ MyCredit/Controllers/HiController.cs
@@ -0,0 +1,27 @@
+﻿using Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MyCredit.Controllers
+{
+    public class HiController : ApiController
+    {
+        Greeter gr = new Greeter();
+        // GET: api/Hi
+        public string  Get()
+        {
+            return gr.SayHello();
+        }   
+               
+
+
+   
+
+
+   
+    }
+}
---
+++ Repositories/Greeter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+   public class Greeter : IGreeter
+    {
+        public string SayHello()
+        {
+            return "Hi there!";
+        }
+    }
+}
